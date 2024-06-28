c9ace6b6abe6d39bb8359a69aa8958c7b31cb3a2
Victor <lessricc@gmail.com>
2024-06-28 18:45:29 +0200

    mejorando poco a poco

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,17 @@
 // El styles lo importamos aquí para que se encargue Vite de compilar todo
-import '../scss/styles.scss';
+import "../scss/styles.scss";
+
+const formElement = document.getElementById("form");
+
+const checkForm = (event) => {
+  event.preventDefault();
+};
+
+formElement.addEventListener("submit", checkForm);
+
+const inputNameElement = document.getElementById("name");
+const inputNumberElement = document.getElementById("number");
+const inputDateElement = document.getElementById("date");
+const inputYearElement = document.getElementById("year");
+const inputCvcElement = document.getElementById("cvc");
+const buttonElement = document.getElementById("button");
